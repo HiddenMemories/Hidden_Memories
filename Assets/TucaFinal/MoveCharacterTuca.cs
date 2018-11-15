b1e55fa7848f476e7eff7836f013d375f8602a97
CaiqueMouraGit <caique.demoura@globomail.com>
2018-11-15 16:11:01 -0200

    New Control

--- Assets/TucaFinal/MoveCharacterTuca.cs
+++ Assets/TucaFinal/MoveCharacterTuca.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharacterState1
+{
+    idle,
+    walking,
+    running,
+    crounching,
+    pushing,
+    dead
+}
+
 public class MoveCharacterTuca : MonoBehaviour
 {
+
+    public CharacterState1 characterState1;
     Rigidbody _rigidbody;
     Animator anim;
 
@@ -11,9 +23,12 @@ public class MoveCharacterTuca : MonoBehaviour
 
     [Tooltip("Character Speed")]
     float speed;
+    public float speedSmoothTime = 0.5f;
 
     public float walkSpeed;
     public float runSpeed;
+
+    Vector3 movement;
 
     void Awake()
     {
@@ -22,34 +37,70 @@ public class MoveCharacterTuca : MonoBehaviour
 
         cam = Camera.main;
 
-        speed = walkSpeed;
+        speed = 0;
     }
 
     void Update()
     {
+
+        #region COMENTADO
+        /*
         #region Movement Character
         Vector3 right = cam.transform.right;
         Vector3 forward = Vector3.Cross(right, Vector3.up);
 
-        float rotate = Input.GetAxis("Horizontal");
+        //  float rotate = Input.GetAxis("Horizontal");
 
-        Vector3 movement = Vector3.zero;
+        // Vector3 movement = Vector3.zero;
 
         movement += right * (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
-        
-        //movement += this.transform.forward * (Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+
+        movement += this.transform.forward * (Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        #endregion       
+
+        transform.Translate(movement * Time.deltaTime * speed);
+        //_rigidbody.MovePosition(transform.position + movement * Time.deltaTime);
+
+        // transform.Rotate(Vector3.up, rotate * 100 * Time.deltaTime);
+        */
         #endregion
 
-        _rigidbody.MovePosition(transform.position + movement * Time.deltaTime);
+        if (movement == Vector3.zero)
+        {
+            speed = 0;
+            characterState1 = CharacterState1.idle;
+        }
 
-        transform.Rotate(Vector3.up, rotate * 100 * Time.deltaTime);
+        if (movement != Vector3.zero)
+        {
+            speed = walkSpeed;
+            characterState1 = CharacterState1.walking;
+        }
 
-        // Animation(movement);
+        if (Input.GetKey(KeyCode.LeftShift) && movement != Vector3.zero)
+        {
+            speed = runSpeed;
+            characterState1 = CharacterState1.running;
+        }
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        print(moveVertical);
+
+        movement = new Vector3(0f, 0f, moveVertical);
+
+        transform.Rotate(Vector3.up, moveHorizontal * 100 * Time.deltaTime);
+        transform.Translate(movement * Time.deltaTime * speed);
+
+        Animation(speed);
     }
 
-    void Animation(Vector3 velocidade)
+    void Animation(float velocidade)
     {
-        anim.SetFloat("SpeedPercent", velocidade.magnitude);
+        anim.SetFloat("SpeedPercent", speed, speedSmoothTime, Time.deltaTime);
 
     }
 }
